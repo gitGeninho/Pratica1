55cfb9b0fdba64b5fa8d717f5b320a904c375651
gitGeninho <38689653+gitGeninho@users.noreply.github.com>
2018-09-13 09:30:22 -0300

    a
    aaaaaaaaaaaaaaa

---
+++ code.js
@@ -0,0 +1,19 @@
+  
+// Declaração das variáveis globais
+        var objCanvas = null;
+        var objContexto = null;
+
+
+
+// Declação dos objetos para representar
+// as imagens
+var imgFundo = new Image();
+    imgFundo.src="img/fundo.png";
+
+var imgHeroi = new Image();
+    imgHeroi.src="img/heroi.png";
+
+ function AtualizaTela()
+ {
+ 	
+ }
