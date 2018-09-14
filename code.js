ff099ff4d89dc943314f3d41b5c3e25ad9573ea0
gitGeninho <38689653+gitGeninho@users.noreply.github.com>
2018-09-14 08:53:12 -0300

    a

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
-  
+﻿  
 // Declaração das variáveis globais
         var objCanvas = null;
         var objContexto = null;
 
-
+//geninho vc é boboooooooooooo
 
 // Declação dos objetos para representar
 // as imagens
@@ -17,10 +17,14 @@ function AtualizaTela()
     objContexto.drawImage(imgFundo,0,0);
  }
 
+// Inicializa variaveis
 function Iniciar()
  {
     objCanvas = document.getElementById("meuCanvas");
     objContexto = objCanvas.getContext("2d");
+
+    //var audio = new Audio('sons/musica.mpo3');
+	//audio.play();
 
     AtualizaTela();
   }
