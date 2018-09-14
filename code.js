5aee15aec8aa6fe4e8bd5dd1f59d28ebd68cdbd7
gitGeninho <38689653+gitGeninho@users.noreply.github.com>
2018-09-14 11:37:08 -0300

    a
    a

--- code.js
+++ code.js
@@ -5,14 +5,20 @@
 
 // Declação dos objetos para representar
 // as imagens
-var imgFundo = new Image();
-    imgFundo.src="img/fundo.png";
+
+/*var imgFundo = new Image();
+    imgFundo.src="img/fundo.png";*/
+
+var imgPers = new Image();
+    imgPers.src="img/bytechan.png";
+
 
 
 // Redesenha a tela
 function AtualizaTela()
  {
-    objContexto.drawImage(imgFundo,0,0);
+   // objContexto.drawImage(imgFundo,0,0);
+    objContexto.drawImage(imgPers,690,430);
  }
 
 // Inicializa variaveis
---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+<center>
+	<h2 id="headerLogin">Login:</h2>
+	<div class="fields">
+		Login:</br>
+		 <input type="" name="">
+
+		Senha:</br>
+		 <input type="" name="">
+	</div>
+</center>   
+    
+</body>
+</html>
