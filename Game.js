3845455d5d98a710b06143f7186d2f78627062ee
gitGeninho <38689653+gitGeninho@users.noreply.github.com>
2018-10-29 10:01:42 -0300

    a
    a

--- Game.js
+++ Game.js
@@ -5,22 +5,21 @@
 // Declação dos objetos para representar
 // as imagens
 
-var imgFundo = new Image();
-    imgFundo.src="img/fundo.png";
-
 var imgPers = new Image();
     imgPers.src = "img/bytechan.png";
 
 var imgBtnPlay = new Image();
     imgBtnPlay.src = "img/btnPlay.png";
-
+var imgBtnInfo = new Image();
+    imgBtnInfo.src="img/btnInfo.png";
 
 
 // Redesenha a tela
-function AtualizaTela()
+function Menu()
  {
     //objContexto.drawImage(imgFundo,0,0);
     objContexto.drawImage(imgBtnPlay,100,100);
+
  }
 
 // Inicializa variaveis
@@ -32,5 +31,5 @@ function Iniciar()
     //var audio = new Audio('sons/musica.mpo3');
 	//audio.play();
 
-    AtualizaTela();
+    Menu();
   }
