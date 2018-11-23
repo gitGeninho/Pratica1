7f155e70059ac4d7545748f4148f037d9f548bd6
gitGeninho <38689653+gitGeninho@users.noreply.github.com>
2018-11-23 11:32:08 -0200

    a

--- Game.js
+++ Game.js
@@ -3,6 +3,7 @@ var pontos = 0;
 var bloco;
 var barra;
 var gameSpeed = 10;
+var audio = new Audio('campainha_escola.mp3');
 
 var myGameArea = {
     canvas : document.createElement("canvas"),
@@ -21,8 +22,11 @@ var myGameArea = {
     
 function updateGameArea() {
     myGameArea.clear();
-    bloco.update();
     barra.update();
+    bloco.update();
+    if(bloco.x==600)
+        audio.play();
+    bloco2.update();
 }
 
 function text() {
@@ -44,16 +48,17 @@ var imgBtnPause = new Image();
     //audio.play();
 
 
-function component(width, height, color, x, y) {
+function component(width, height, color, x, y, speed) {
     this.width = width;
     this.height = height;
     this.x = x;
     this.y = y; 
     ctx = myGameArea.context;
     ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.fillRect(this.x, this.y, this.width, this.height, this.movel);
     this.update = function(){
-        this.x += gameSpeed;
+        
+        this.x += speed;   
         ctx = myGameArea.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
@@ -68,12 +73,13 @@ function Iniciar()
 
     myGameArea.start();
 
-    barra = new component(50, 850,"lightgray", 650, 00);
-    bloco = new component(30, 30, "red", 00, 300);
-    context.beginPath();
+    barra  = new component(50, 850,"lightgray", 650, 00, 0);
+    bloco  = new component(30, 30, "red", 00, 300, gameSpeed);
+    bloco2 = new component(30, 30, "red",-50, 50, gameSpeed/3)
+   /* context.beginPath();
     context.moveTo(100, 150);
     context.lineTo(450, 50);
-    context.stroke();
+    context.stroke();*/
     
   //  Menu();
   }
@@ -81,17 +87,59 @@ function Iniciar()
 function clicarSpace(event){
     var tecla = event.keyCode; 
     alert(bloco.x);
-    if(tecla == 32){
-        if(bloco.x >= 650 && bloco.x <= 750){
-            bloco.x = 0;
-            pontos += 50;
+    if(tecla == 32)
+    {
+        
+        if(bloco.x>750)
+        {
+            bloco.x=0;
+            pontos -= 30;
             document.getElementById("pontos").innerHTML = pontos;
+        }    
             
-        }
-        else{
-            bloco.x = 0;
+        
+        if(bloco2.x>750)
+        {
+            bloco2=0;
             pontos -= 30;
             document.getElementById("pontos").innerHTML = pontos;
         }
-    }
+
+
+        if(bloco.x>bloco2.x)
+        {
+            if((bloco.x >= 650 && bloco.x <= 700))
+            {
+                bloco.x  = 0;
+                pontos += 50;
+                document.getElementById("pontos").innerHTML = pontos;            
+            }
+            else
+            {
+                bloco.x  = 0;
+                pontos -= 30;
+                document.getElementById("pontos").innerHTML = pontos;
+            }
+            return;
+        }
+    
+
+        if((bloco2.x >= 650 && bloco2.x <= 700))
+        {
+        bloco2.x  = 0;
+        pontos += 50;
+        document.getElementById("pontos").innerHTML = pontos;
+        
+        }
+        else{
+            bloco2.x  = 0; 
+            pontos -= 30;
+            document.getElementById("pontos").innerHTML = pontos;
+        }            
+      
+        
+
+    }   
+        
+    
 }
